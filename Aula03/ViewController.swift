36d764c188a2550a19ee9acfadcadd0452db1bd9
Diogo Ribeiro de Oliveira <diogoro@Diogos-Mac.local>
2014-12-15 21:50:28 -0300

    ajustes no projeto

--- Aula03/ViewController.swift
+++ Aula03/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelSaudacao: UILabel!
+    @IBOutlet weak var labelNome: UILabel!
+    
+    @IBOutlet weak var textSaudacao: UITextField!
+    
+    @IBOutlet weak var textNome: UITextField!
+    
+    @IBOutlet weak var botao: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func modificarLabel(sender: UIButton) {
+        labelSaudacao.hidden = false
+        labelNome.hidden = false
+        labelSaudacao.text = textSaudacao.text
+        labelNome.text = textNome.text
+        textNome.text = ""
+        textSaudacao.text = ""
+        textSaudacao.resignFirstResponder()
+        textNome.resignFirstResponder()
+        
+        labelNome.textColor = UIColor(red: 0.7, green: 0.5, blue: 0.8, alpha: 1.0)
+        
+        //mudar o nome do botao
+        botao.setTitle("Modificar :P", forState: UIControlState.Normal)
+        botao.setTitle("Modificar Precionado", forState: UIControlState.Highlighted)
+    }
 
 }
 
